57c04ca939f32a48f424a4c227ad5c5146fcf370
nazar-trofumyk <gematogey@gmail.com>
2025-06-19 15:22:33 +0300

    1-commit

--- src/components/Details-Info-Copmonents/DetailsInfoComponent.tsx
+++ src/components/Details-Info-Copmonents/DetailsInfoComponent.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 
 import {IMovie} from "@/models/IMovie";
+import './Details.css'
 type MoviePropsType = {
     movie:IMovie
 }
@@ -9,9 +10,21 @@ const DetailsInfoComponent:FC<MoviePropsType> =  ({movie}) => {
 
 
     return (
-        <div>
-            {movie.id} === {movie.title}
-        </div>
+
+            <div className={"box-details"}>
+
+                <img alt={'movie-img'} className={'movie-details-img'} src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
+                <div className={'description'}>
+                    <h1>{movie.title}</h1>
+                    <h2>{movie.overview}</h2>
+                    <ul className={"ul-details"}>
+                        <li className={"li-details"}><span className={"span-list"}>Release Date</span><hr/><span className={"uppercase"}>{movie.release_date}</span></li>
+                        <li className={"li-details"}><span className={"span-list"}>Original Language</span><hr/><span className={"uppercase"}>{movie.original_language}</span></li>
+                        <li className={"li-details"}><span className={"span-list"}>Original Title</span><hr/><span className={"uppercase"}>{movie.original_title}</span></li>
+                    </ul>
+                </div>
+            </div>
+
     );
 };
 
