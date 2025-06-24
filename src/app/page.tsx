f7fd726f7d3a30656a96942b5810c56192c3a04a
nazar-trofumyk <gematogey@gmail.com>
2025-06-24 12:08:45 +0300

    1-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import GenresComponents from "@/components/Genre/GenresComponents";
+import MainComponent from "@/components/Home-Component/MainComponent/MainComponent";
 
 const HomePage = () => {
     return (
         <div>
 
 <GenresComponents/>
-            Home
+            <MainComponent/>
 
         </div>
     );
